a8e1fb854e58b61eafeddf28192313f951bce402
Trung Lam <trunglam107@gmail.com>
2023-04-27 22:51:58 -0700

    test hidden api

--- src/backend/Firebase.js
+++ src/backend/Firebase.js
@@ -10,7 +10,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: `${process.env.FIREBASE_API_KEY}`,
   authDomain: "dog-rating.firebaseapp.com",
   projectId: "dog-rating",
   storageBucket: "dog-rating.appspot.com",
